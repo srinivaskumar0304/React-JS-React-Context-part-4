c3149bc4eed120ba67da6ccae47e28acc2144183
Srinivas kumar <seenukumar124@gmail.com>
2022-11-24 04:01:30 +0000

    React JS React Context part 4

---
+++ src/components/Cart/index.js
@@ -0,0 +1,34 @@
+import Header from '../Header'
+
+import CartContext from '../../context/CartContext'
+
+import EmptyCartView from '../EmptyCartView'
+
+import CartListView from '../CartListView'
+
+import './index.css'
+
+const Cart = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {cartList} = value
+      const showEmptyCartView = cartList.length === 0
+      return (
+        <>
+          <Header />
+          {showEmptyCartView ? (
+            <EmptyCartView />
+          ) : (
+            <div className="cart-container">
+              <div className="cart-content-container">
+                <h1 className="cart-heading">My Cart</h1>
+                <CartListView />
+              </div>
+            </div>
+          )}
+        </>
+      )
+    }}
+  </CartContext.Consumer>
+)
+export default Cart
